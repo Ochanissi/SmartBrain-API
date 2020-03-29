8bac614e4bdbfdc007bc3340f6cf584c18f3a5b3
Mirel Home <mirelalex2094@gmail.com>
2020-03-29 19:28:38 +0300

    clearing the console logs

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -2,8 +2,6 @@ const redisClient = require('./signin').redisClient;
 
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log('3');
-  console.log(req.headers);
   if (!authorization) {
     return res.status(401).send('Unauthorized');
   }
