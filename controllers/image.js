07376b71170375a920feab4afacf47db43607ab4
Mirel Home <mirelalex2094@gmail.com>
2020-06-21 17:56:45 +0300

    fixed the API, since they migrated the face model api to a different model

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,11 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict(
+      // Clarifai.FACE_DETECT_MODEL,
+      'c0c0ac362b03416da06ab3fa36fb58e3',
+      req.body.input
+    )
     .then((data) => {
       res.json(data);
     })
